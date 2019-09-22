f4925e4c0ee21d4c2955fb7b90ac8d844ab7637a
yanjinhai <jinhaiyan777@gmail.com>
2019-09-22 11:48:16 -0700

    minor changes to comments

--- drive_demo_2.py
+++ drive_demo_2.py
@@ -8,13 +8,17 @@ import sys
 drive_motor_1 = Motor(OUTPUT_B)
 drive_motor_2 = Motor(OUTPUT_C)
 
+#Define drive function
 def drive(pos, spd, md1, md2):
     '''
-    Moves the robot forward, backward, or turns in place
+    Moves the robot forward, backward, or turns in place.
 
     pos = number of degrees each motor turns
+
     spd = absolute speed of both motors
+    
     md1 = movement direction of drive_motor_1 (1 or -1)
+    
     md2 = movement direction of drive_motor_2 (1 or -1)
     '''
     
