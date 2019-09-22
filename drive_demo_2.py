bc451beba74cbcaa4792aaa7f05e46eca2055f2c
yanjinhai <jinhaiyan777@gmail.com>
2019-09-22 11:38:25 -0700

    added an error handler to drive_demo_2

--- drive_demo_2.py
+++ drive_demo_2.py
@@ -2,6 +2,7 @@
 '''More advanced demo on driving robot movement'''
 
 from ev3dev.ev3 import *
+import sys
 
 #Initialize drive motors
 drive_motor_1 = Motor(OUTPUT_B)
@@ -13,11 +14,20 @@ def drive(pos, spd, md1, md2):
 
     pos = number of degrees each motor turns
     spd = absolute speed of both motors
-    md1 = movement direction of drive_motor_1
-    md2 = movement direction of drive_motor_2
+    md1 = movement direction of drive_motor_1 (1 or -1)
+    md2 = movement direction of drive_motor_2 (1 or -1)
     '''
     
-    #Run the motors. 
+    #If something doesn't make sense, exit the program
+    if spd > 1000 or spd < -1000:
+        print("Error: You can't have a speed above 1000 or below -1000!")
+        sys.exit()
+    if (md1 != 1 and md1 != -1) or (md2 != 1 and md2 != -1):
+        print('Error: The movement direction of a motor should be either 1(forward) or -1(backward).')
+        sys.exit()
+    
+
+    #Run the motors
     drive_motor_1.run_to_rel_pos(position_sp = pos, speed_sp = spd * md1)
     drive_motor_2.run_to_rel_pos(position_sp = pos, speed_sp = spd * md2)
 
